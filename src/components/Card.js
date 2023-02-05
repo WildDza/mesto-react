b766a10aebe8ea5b71c3b6c8289ecff32d41f3ff
LAPTOP-CNI641BD\WildD <Kleha91@yandex.ru>
2023-02-06 00:10:23 +0700

    fix: тестовые изменения

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ function Card(props) {
     <div id="post-template">
       <article className="post" id={props.id}>
         <img className="post__img" src={props.link} alt={props.name} onClick={handleClick} />
-        <button className="post__delete" type="button" aria-label="Иконка удаления поста"></button>
+        <button className="post__delete" type="button" aria-label="Иконка удаления поста" />
         <div className="post__info">
           <h2 className="post__title">{props.name}</h2>
           <div className="post__like">
